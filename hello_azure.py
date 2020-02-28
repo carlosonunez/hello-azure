75922f50f82760f3558f65061b5788a3edb0afd7
Carlos Nunez <carlosonunez@noreply.github.com>
2020-02-27 18:18:31 -0600

    Increment the counter through the DB
    While the counter can be created client-side, this is being done to
    better demonstrate a three-tier web application.

--- hello_azure.py
+++ hello_azure.py
@@ -1,7 +1,7 @@
-import os
-from flask import Flask, render_template
+import os, random, string
+from flask import Flask, render_template, make_response, request
 from flask_sqlalchemy_session import flask_scoped_session
-from sqlalchemy import create_engine, Column, Integer, MetaData, Table
+from sqlalchemy import create_engine, Column, Integer, MetaData, Table, String
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -9,14 +9,25 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 class User(Base):
     __tablename__ = 'click_data'
-    click_count = Column(Integer, primary_key=True)
+    id = Column(String, primary_key=True)
+    click_count = Column(Integer)
 
+def generate_id():
+    return ''.join(random.choice(string.ascii_lowercase + \
+            string.ascii_uppercase + \
+            string.digits) for _ in range(16))
 
 def create_table_if_nonexistent(engine, table_name):
     if not engine.has_table(table_name):
         metadata = MetaData(engine)
-        Table(table_name, metadata, Column('click_count', Integer, primary_key=True))
+        Table(table_name, metadata,
+                Column('id', String, primary_key=True),
+                Column('click_count', Integer))
         metadata.create_all()
+
+def fetch_click_count(session, session_id):
+    user = session.query(User).get(session_id)
+    return 0 if user is None else user.click_count
 
 hello_azure_app = Flask(__name__)
 session_conn_string = str.format("postgres://{0}:{1}@{2}:{3}/sessions",
@@ -31,24 +42,27 @@ session = flask_scoped_session(session_factory, hello_azure_app)
 
 @hello_azure_app.route('/click', methods=['POST'])
 def click():
-    num_clicks = clicks()
-    if num_clicks == 0:
-        new_user = User(0)
-        session.add(new_user)
+    session_id = request.cookies.get('session_id')
+    current_user = session.query(User).get(session_id)
+    if current_user is None:
+        session.add(User(id=session_id, click_count=1))
         session.commit()
+        return '1', 201
     else:
-        session.query(User).update({"click_count": num_clicks + 1})
+        current_user.click_count = current_user.click_count + 1
         session.commit()
-    return '', 204
+        return clicks()
 
 @hello_azure_app.route('/clicks')
 def clicks():
-    user = session.query(User)
-    return '0' if user.count() == 0 else str(user.first().click_count)
+    session_id = request.cookies.get('session_id')
+    return str(fetch_click_count(session, session_id))
 
 @hello_azure_app.route('/')
 def index():
-    return render_template('index.html.j2')
+    response = make_response(render_template('index.html.j2'))
+    response.set_cookie('session_id', generate_id(), max_age=120)
+    return response
 
 hello_azure_app.run(host=os.environ.get('FLASK_HOST'),
                     port=os.environ.get('FLASK_PORT'))
