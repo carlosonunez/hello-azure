5483518060cbd4a27bed437ab6cd3ae6b1405d5b
Carlos Nunez <carlosonunez@noreply.github.com>
2020-02-27 18:18:18 -0600

    Vendor these

--- static/page.js
+++ static/page.js
@@ -1,13 +1,29 @@
-$( "#click_count" ).ready(function() {
-  $.ajax({
-    url: "/clicks",
-    type: "GET",
-    dataType: "html",
-    success: function(data) {
-      $('#click_count').text(data);
-    },
-    error: function(xhr, status) {
-      console.log("Unable to fetch click count.");
-    }
+$( document ).ready(function() {
+  $( "#click_count" ).ready(function() {
+    $.ajax({
+      url: "/clicks",
+      type: "GET",
+      dataType: "html",
+      success: function(data) {
+        $('#click_count').text(data);
+      },
+      error: function(xhr, status) {
+        console.log("Unable to fetch click count.");
+      }
+    });
   });
+
+  $( "#clicker" ).click(function() {
+    $.ajax({
+      url: "/click",
+      type: "POST",
+      dataType: "html",
+      success: function(data) {
+        $('#click_count').text(data);
+      },
+      error: function(xhr, status) {
+        console.log("Unable to update click count.");
+      }
+    });
+  })
 });
