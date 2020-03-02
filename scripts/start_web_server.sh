b5b0db472f934c80e4974bb30fb63cf3c11ee4ca
Carlos Nunez <carlosonunez@noreply.github.com>
2020-03-02 00:38:13 -0600

    Ensure Docker network is started

--- scripts/start_web_server.sh
+++ scripts/start_web_server.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 docker-compose up -d database blobstore && \
-  docker-compose run --service-port --rm init-database && \
-  docker-compose run --service-port --rm init-blobstore && \
-  docker-compose up hello-azure
+  docker-compose run --service-ports --rm init-database && \
+  docker-compose run --service-ports --rm init-blobstore && \
+  docker-compose build hello-azure && \
+  docker-compose run --service-ports --rm hello-azure
