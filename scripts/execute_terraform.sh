7d1380051909d0eec280f1bc8aa016459ecd693d
Carlos Nunez <carlosonunez@noreply.github.com>
2020-03-02 18:24:08 -0600

    Add scaffolding to support Terraform.

---
+++ scripts/execute_terraform.sh
@@ -0,0 +1,11 @@
+#!sh
+set -x
+
+init_terraform() {
+  terraform init -backend-config="resource_group_name=terraform" \
+    -backend-config="storage_account_name=${TERRAFORM_STATE_STORAGE_ACCOUNT_NAME}" \
+    -backend-config="container_name=${TERRAFORM_STATE_STORAGE_CONTAINER_NAME}" \
+    -backend-config="key=tfstate"
+}
+
+init_terraform && terraform $*
