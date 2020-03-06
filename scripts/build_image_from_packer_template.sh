77fbf719f1fff1ffa68f4ebe223f368aeb082626
Carlos Nunez <carlosonunez@noreply.github.com>
2020-03-06 11:51:20 -0600

    Don't error if images already exist in blob storage

--- scripts/build_image_from_packer_template.sh
+++ scripts/build_image_from_packer_template.sh
@@ -7,5 +7,16 @@ then
   exit 1
 fi
 
+# Packer will error if the images created by the template already exist
+# in the destination repository. The code below works around this.
 cat "$PACKER_TEMPLATE" | yq . > /tmp/template.json && \
-  packer build /tmp/template.json
+  packer build -parallel -parallel-builds=0 \
+    -machine-readable /tmp/template.json | tee /tmp/result.log
+errors_encountered=$(cat /tmp/result.log | \
+  egrep 'Build .* errored:' | \
+  egrep -v 'the managed image named .* already exists in the resource group')
+if ! test -z "$errors_encountered"
+then
+  >&2 echo "ERROR: Packer encountered these errors: $errors_encountered"
+  exit 1
+fi
