254d322ccf0410c87595cc219ba683e1f4c9787e
Carlos Nunez <carlosonunez@noreply.github.com>
2020-03-05 18:00:28 -0600

    Add deployment script

---
+++ scripts/deploy.sh
@@ -0,0 +1,109 @@
+#!/usr/bin/env bash
+deploy_infrastructure() {
+  docker-compose run --rm terraform apply
+}
+
+verify_that_all_secrets_populated() {
+  for secret in azure_storage_account_key \
+      azure_storage_account_name \
+      azure_storage_endpoint \
+      common_private_key \
+      databases \
+      machine_user \
+      postgres_password \
+      postgres_user \
+      webservers
+  do
+    file="secrets/${secret}"
+    if ! test -f "$file" || test -z "$file"
+    then
+      >&2 echo "ERROR: Secret not found or populated: $file"
+      exit 1
+    fi
+  done
+}
+
+build_ansible_inventory() {
+  cat >inventory <<-INVENTORY_FROM_TERRAFORM
+[all]
+$(cat secrets/webservers)
+$(cat secrets/databases)
+
+[webservers]
+$(cat secrets/webservers)
+
+[databases]
+$(cat secrets/databases)
+INVENTORY_FROM_TERRAFORM
+}
+
+copy_images_to_storage_container() {
+  endpoint="AccountName=$(cat secrets/azure_storage_account_name);\
+  AccountKey=$(cat secrets/azure_storage_account_key);\
+  BlobEndpoint=$(cat secrets/azure_storage_endpoint)/$(cat secrets/azure_storage_account_name)"
+
+  for image in $(find $PWD/static/*.png)
+  do
+    blob_name=$(basename "$image")
+    if [ "$blob_name" != "logo.png" ]
+    then
+      path="/workdir/static/$blob_name"
+      >&2 echo "INFO: Uploading '$image'"
+      docker-compose run --rm \
+        -v $PWD:/workdir \
+        -w /workdir \
+        azcli storage blob upload --file "$path" --container 'app-images' \
+          --name "$blob_name" \
+          --account-name "$(cat secrets/azure_storage_account_name)" \
+          --account-key "$(cat secrets/azure_storage_account_key)" || exit 1
+    fi
+  done
+}
+
+create_env_file() {
+  cp .env.test .env && \
+    gsed -i "s#\(POSTGRES_USER\)=.*\$#\1=$(cat secrets/postgres_user)#" .env && \
+    gsed -i "s#\(POSTGRES_PASSWORD\)=.*\$#\1=$(cat secrets/postgres_password)#" .env && \
+    gsed -i "s#\(SESSION_DB_USER\)=.*\$#\1=$(cat secrets/postgres_user)#" .env && \
+    gsed -i "s#\(SESSION_DB_PASSWORD\)=.*\$#\1=$(cat secrets/postgres_password)#" .env && \
+    gsed -i "s#\(SESSION_DB_HOST\)=.*\$#\1=$(cat secrets/databases | head -1)#" .env && \
+    gsed -i "s#\(AZURE_STORAGE_ACCOUNT_NAME\)=.*\$#\1=$(cat secrets/azure_storage_account_name)#" .env && \
+    gsed -i "s#\(AZURE_STORAGE_ACCOUNT_KEY\)=.*\$#\1=$(cat secrets/azure_storage_account_key)#" .env && \
+    gsed -i "s#\(AZURE_STORAGE_ENDPOINT\)=.*\$#\1=$(cat secrets/azure_storage_endpoint)#" .env
+}
+
+tighten_access_to_private_key() {
+  chmod 600 secrets/common_private_key
+}
+
+copy_env_file_to_servers() {
+  docker-compose run --entrypoint ansible \
+    -v $PWD:/app \
+    -w /app \
+    ansible-with-systemd -i inventory \
+      --module-name copy \
+      --become \
+      --args "src=.env dest=/.env" \
+      --user "$(cat secrets/machine_user)" \
+      --private-key secrets/common_private_key \
+      all
+}
+
+deploy_app() {
+  env $(cat $PWD/.env | grep -v '#' | tr '\n' ' ') \
+  docker-compose run -v $PWD:/app -w /app \
+      --entrypoint ansible-playbook ansible-with-systemd \
+      -i inventory \
+      -u "$(cat secrets/machine_user)" \
+      --private-key "$(cat secrets/common_private_key)" \
+      app.yml
+}
+
+#deploy_infrastructure && \
+#  verify_that_all_secrets_populated && \
+#  create_env_file
+#  copy_images_to_storage_container &&\
+  tighten_access_to_private_key && \
+  build_ansible_inventory && \
+  copy_env_file_to_servers && \
+  deploy_app
