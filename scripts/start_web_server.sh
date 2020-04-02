57b916f159c3831df171b9fe35f50ebae6da2755
Carlos Nunez <carlosonunez@noreply.github.com>
2020-04-02 10:40:53 -0500

    Refactor script to make functions smaller

--- scripts/start_web_server.sh
+++ scripts/start_web_server.sh
@@ -2,8 +2,6 @@
 PERSISTENCE_SERVICES="blobstore database graf_database prom_database graf_cache"
 MONITORING_SERVICES="grafana prometheus"
 INIT_SERVICES="init-database init-blobstore"
-PROMETHEUS_PORT=$(get_exposed_port "prometheus")
-GRAFANA_PORT=$(get_exposed_port "grafana")
 
 get_exposed_port() {
   if ! which yq > /dev/null
@@ -11,30 +9,32 @@ get_exposed_port() {
     >&2 echo "ERROR: yq not installed."
     exit 1
   fi
-  docker-compose config | yq -r '.services | \
-    to_entries | \
-    .[] | select(.key == "prometheus") | \
+  docker-compose config | yq -r '.services |
+    to_entries |
+    .[] | select(.key == "prometheus") |
     .value.ports[0]' | \
     cut -f1 -d ':'
 }
 
 ensure_persistence_started() {
-  for service in "$PERSISTENCE_SERVICES"
+  for service in $PERSISTENCE_SERVICES
   do
     if ! docker-compose ps "$service" | grep -q "Up"
     then
-      >&2 echo "ERROR: $service failed to start."
+      >&2 echo "ERROR: $service failed to start. \
+Run 'docker-compose logs $service' for more information."
       exit 1
     fi
   done
 }
 
 ensure_monitoring_started() {
-  for service in "$MONITORING_SERVICES"
+  for service in $MONITORING_SERVICES
   do
     if ! docker-compose ps "$service" | grep -q "Up"
     then
-      >&2 echo "ERROR: $service failed to start."
+      >&2 echo "ERROR: $service failed to start. \
+Run 'docker-compose logs $service' for more information."
       exit 1
     fi
   done
@@ -45,9 +45,9 @@ wait_for_persistence() {
   seconds_elapsed=0
   while true
   do
-    docker-compose --log-level INFO run --service-ports --rm graf_network_check && \
-      docker-compose --log-level INFO run --service-ports --rm prom_network_check && \
-      docker-compose --log-level INFO run --service-ports --rm app_network_check && \
+    docker-compose --log-level ERROR run --service-ports --rm graf_persistence_check && \
+      docker-compose --log-level ERROR run --service-ports --rm prom_persistence_check && \
+      docker-compose --log-level ERROR run --service-ports --rm app_persistence_check && \
       break
     sleep 1
     if [ "$seconds_elapsed" == "$max_seconds_to_wait" ]
@@ -64,8 +64,8 @@ wait_for_monitoring() {
   seconds_elapsed=0
   while true
   do
-    nc -z "$(get_exposed_port "prometheus")" && \
-      nc -z "$(get_exposed_port "grafana")" && \
+    docker-compose --log-level ERROR run --service-ports --rm graf_monitoring_check && \
+      docker-compose --log-level ERROR run --service-ports --rm prom_monitoring_check && \
       break
     sleep 1
     if [ "$seconds_elapsed" == "$max_seconds_to_wait" ]
@@ -77,11 +77,30 @@ wait_for_monitoring() {
   done
 }
 
-docker-compose up -d "$PERSISTENCE_SERVICES" && \
-  ensure_persistence_started && \
-  wait_for_persistence && \
-  docker-compose up -d "$MONITORING_SERVICES" && \
-  ensure_monitoring_started && \
-  wait_for_monitoring && \
-  docker-compose run --service-ports --rm "$INIT_SERVICES" && \
-  docker-compose up --build -d hello-azure
+start_persistence() {
+  docker-compose up -d $PERSISTENCE_SERVICES && \
+    ensure_persistence_started && \
+    wait_for_persistence
+}
+
+start_monitoring() {
+  docker-compose up -d $MONITORING_SERVICES && \
+    ensure_monitoring_started && \
+    wait_for_monitoring
+}
+
+initialize_app() {
+  for service in $INIT_SERVICES
+  do
+    docker-compose run --rm --service-ports $service
+  done
+}
+
+start_app() {
+  docker-compose up -d hello-azure
+}
+
+start_persistence && \
+  start_monitoring && \
+  initialize_app && \
+  start_app
