ef89c6acf64be76b8b5e896dec372db3791888da
Carlos Nunez <carlosonunez@noreply.github.com>
2020-07-02 16:11:21 -0500

    Host k3s on Vagrant
    This commit provides code needed to create a k3s cluster with Vagrant.
    This does _not_ include the Ansible playbooks that actually provision
    it.
    
    All the work I did to do this is below:
    
    Add a script to provision a virtualized k3s cluster.
    
    apply Vagrant
    
    Make exec
    
    Fix syntax error.
    
    Remove vagrant and tf metadata.
    
    Remove vagrant and tf metadata.
    
    REALLY delete Vagrant metadata.
    
    Add vagrantfile for our k3s nodes.
    
    Add script for destroying k3s nodes.
    
    No prompts
    
    Support reprovisioning machines.
    
    Shouldn't start at .1
    
    Add script for getting Git toplevel (used more than 3 times)
    
    Add shortcut for launching Vagrant for k3s
    
    Circular dependency
    
    squash with shortcuts
    
    Yay logic errors
    
    Not supported by Vagrant: https://www.vagrantup.com/docs/providers/virtualbox/usage.html
    
    Don't commit log files
    
    Use our own SSH key
    
    This will make working with Ansible easier without having to use a SSH
    password. The key is refreshed with every new cluster and is never
    committed into source control.
    
    No passphrase.
    
    Fix relative pathing
    
    undo
    
    Don't commit public key either
    
    Have to add our key into auth keys manually
    
    We don't use it for provisioning; we simply add it

---
+++ scripts/install_k3s.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+INSTALL_DIR=$PWD/.k3s
+INSTALL_K3S_BIN_DIR=$INSTALL_DIR
+
+
+create_local_install_dir() {
+}
+
+ensure_cluster_is_running() {
+}
+
+install_k3s() {
+}
+
+install_k3s && ensure_cluster_is_running
