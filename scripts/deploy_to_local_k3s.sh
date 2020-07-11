ebc95071b89e10d9c6db9ac1312d3a7982bf8cc7
Carlos Nunez <carlosonunez@noreply.github.com>
2020-07-11 08:28:47 -0500

    Add Helm secrets from environment file
    Getting secrets from environment variables into Helm is a
    three-step process. Here is how this works:
    
    1. First, we copy the environment file into the Helm chart. This file is
       git-ignored to prevent us from accidentally committing it. We need to
       do this because Helm charts will not source files from outside of
       their own directory structure by design.
    
    2. Next, we templatize Kubernetes secrets from this file. The
       entire environment file is read into a single secret,
       `{{ .Release.Name }}-secrets`. (In this case, `.Release.Name :=
       "hello-azure"`.)
    
    3. Finally, our `Deployment` sources this as an environment variable
       using `secretKeyRef`.
    
    A tad bit more complicated than Docker Compose, but I understand the
    complexity.

--- scripts/deploy_to_local_k3s.sh
+++ scripts/deploy_to_local_k3s.sh
@@ -19,13 +19,23 @@ push_into_private_registry() {
   docker push "${DOCKER_REGISTRY_LOCATION}/${DOCKER_IMAGE_NAME}"
 }
 
+copy_environment_file_into_helm_chart() {
+  # This file should be gitignored; confirm with `git status` to ensure
+  # that this is the case.
+  cp .env.test helm/hello-azure/
+}
+
 deploy_helm_chart() {
-  helm --kubeconfig=/tmp/k3s-kubeconfig install "$HELM_CHART_NAME" "${TOPLEVEL}/helm/hello-azure"
+  helm --kubeconfig=/tmp/k3s-kubeconfig install \
+    --set environment_file=".env.test" \
+    "$HELM_CHART_NAME" \
+    "${TOPLEVEL}/helm/hello-azure"
 }
 
 if ! (provision_local_k3s_cluster_and_registry &&
   build_docker_image &&
   push_into_private_registry &&
+  copy_environment_file_into_helm_chart &&
   deploy_helm_chart)
 then
   >&2 echo "ERROR: Failed to deploy; see logs."
